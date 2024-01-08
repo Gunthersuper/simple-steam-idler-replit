43ff4ff2e5099fa70cc46bc4d0b0800a5194d181
Gunther <62240649+Gunthersuper@users.noreply.github.com>
2024-01-08 20:31:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ user.on('loggedOn', () => {
 	user.setPersona(status);               
 	user.gamesPlayed(games);
 });
+
+
+// var username2 = process.env.username2;
+// var password2 = process.env.password2;
+// var shared_secret2 = process.env.shared2;
+
+// var games2 = [730, 440, 570, 304930];  // Enter here AppIDs of the needed games
+// var status2 = 1;  // 1 - online, 7 - invisible
+
+
+// user2 = new steamUser();
+// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
+// user2.on('loggedOn', () => {
+// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
+// 	user2.setPersona(status2);               
+// 	user2.gamesPlayed(games2);
+// });
