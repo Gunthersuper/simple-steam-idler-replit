cf1827ca872786c2f5c8d49bc086e01f803b0eae
archonn1337 <156927852+archonn1337@users.noreply.github.com>
2024-02-19 02:47:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,23 +40,3 @@ user.on('loggedOn', () => {
  	user2.setPersona(status2);               
  	user2.gamesPlayed(games2);
  });
-
-
- var username3 = process.env.username3;
- var password3 = process.env.password3;
- var shared_secret3 = process.env.shared3;
-
- var games3 = [730, 570, 440, 1222670, 578080, 1049590, 1928420, 109600, 1172470,
-               714010, 291550, 766570, 236390, 230410, 933110, 397900, 386180, 702320,
-               238960, 1286830, 1677740, 2073850, 1085660, 335240, 304930, 1782210, 1997040,
-               1714320, 438100, 1599340, 8500];  // Enter here AppIDs of the needed games
- var status3 = 1;  // 1 - online, 7 - invisible
-
-
- user3 = new steamUser();
- user3.logOn({"accountName": username3, "password": password3, "twoFactorCode": steamTotp.generateAuthCode(shared_secret3)});
- user3.on('loggedOn', () => {
-	if (user3.steamID != null) console.log(user3.steamID + ' - Successfully logged on');
- 	user3.setPersona(status3);               
- 	user3.gamesPlayed(games3);
- });
